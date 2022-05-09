b4d5ae5fe15f6404d137dbfa2fa7e45e9d5236a5
Jeel <45571080+j33l@users.noreply.github.com>
2022-05-09 00:17:35 -0700

    from Mrunal's repo to here

--- src/index.js
+++ src/index.js
@@ -1,69 +1,17 @@
-import React, { useReducer, useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { firebase } from './firebase/firebase'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// actions
-import { logout, login } from './actions/auth'
-
-// components
-import LoginPage from './components/LoginPage'
-// import ActivityIndicator from './components/ActivityIndicator'
-
-// context
-import AppContext from './context/AppContext'
-
-// reducer
-import authReducer from './reducers/authReducer'
-
-// styles
-import './index.css'
-
-// for use of login/logout first rendering at `onAuthChange` code
-let userDispatchOuter = undefined
-
-const App = () => {
-    const [isLoadingData, setLoadingData] = useState(true)
-
-    const [user, userDispatch] = useReducer(authReducer, {})
-
-    userDispatchOuter = userDispatch
-
-    return (
-        <div>
-            <AppContext.Provider
-                value={{
-                    user,
-                    userDispatch,
-                    isLoadingData,
-                }}
-            >
-                <h1>Hello Worlds!</h1>
-                <p>{user.uid}</p>
-                <LoginPage />
-            </AppContext.Provider>
-        </div>
-    )
-}
-
-let hasRendered = false
-const renderApp = () => {
-    if (!hasRendered) {
-        ReactDOM.render(<App />, document.getElementById('root'))
-        hasRendered = true
-    } else {
-        ReactDOM.render(<LoginPage />, document.getElementById('root'))
-    }
-}
-
-ReactDOM.render(<h1>Loading...</h1>, document.getElementById('root'))
-
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        renderApp()
-        userDispatchOuter(login(user.uid))
-    } else {
-        renderApp()
-        userDispatchOuter(logout())
-    }
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
