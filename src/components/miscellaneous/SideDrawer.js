5f549b58be73fda1362baf76cf78d6430b3adfae
Jeel <45571080+j33l@users.noreply.github.com>
2022-05-19 03:14:09 -0700

    now made API URL with PORT easily changable using env variables

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -1,69 +1,70 @@
-import { Button } from "@chakra-ui/button";
-import { Box, Text } from "@chakra-ui/layout";
+import { Button } from '@chakra-ui/button'
+import { Box, Text } from '@chakra-ui/layout'
 import {
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-} from "@chakra-ui/menu";
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
+} from '@chakra-ui/menu'
 
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Avatar } from "@chakra-ui/avatar";
-import { useHistory } from "react-router-dom";
-import ProfileModal from "./ProfileModal";
-import { ChatState } from "../../Context/ChatProvider";
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import { Avatar } from '@chakra-ui/avatar'
+import { useHistory } from 'react-router-dom'
+import ProfileModal from './ProfileModal'
+import { ChatState } from '../../Context/ChatProvider'
 
 function SideDrawer() {
+    const { user } = ChatState()
 
-  const {
-    user,
-  } = ChatState();
+    const history = useHistory()
 
-  const history = useHistory();
+    const logoutHandler = () => {
+        localStorage.removeItem('userInfo')
+        history.push('/')
+        // history.go();
+    }
 
-  const logoutHandler = () => {
-    localStorage.removeItem("userInfo");
-    history.push("/");
-    history.go();
-  };
-
-  return (
-    <>
-      <Box
-        d="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        bg="white"
-        w="100%"
-        p="5px 10px 5px 10px"
-        borderWidth="5px"
-      >
-        <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
-        </Text>
-        <div>
-          <Menu>
-            <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
-              <Avatar
-                size="sm"
-                cursor="pointer"
-                name={user.name}
-                src={user.pic}
-              />
-            </MenuButton>
-            <MenuList>
-              <ProfileModal user={user}>
-                <MenuItem>My Profile</MenuItem>{" "}
-              </ProfileModal>
-              <MenuDivider />
-              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        </div>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box
+                d="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                bg="white"
+                w="100%"
+                p="5px 10px 5px 10px"
+                borderWidth="5px"
+            >
+                <Text fontSize="2xl" fontFamily="Work sans">
+                    Talk-A-Tive
+                </Text>
+                <div>
+                    <Menu>
+                        <MenuButton
+                            as={Button}
+                            bg="white"
+                            rightIcon={<ChevronDownIcon />}
+                        >
+                            <Avatar
+                                size="sm"
+                                cursor="pointer"
+                                name={user.name}
+                                src={user.pic}
+                            />
+                        </MenuButton>
+                        <MenuList>
+                            <ProfileModal user={user}>
+                                <MenuItem>My Profile</MenuItem>{' '}
+                            </ProfileModal>
+                            <MenuDivider />
+                            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </div>
+            </Box>
+        </>
+    )
 }
 
-export default SideDrawer;
+export default SideDrawer
