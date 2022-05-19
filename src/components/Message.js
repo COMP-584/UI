bd6a62fc74f40fad314b8280ed07eca041944a38
Jeel <45571080+j33l@users.noreply.github.com>
2022-05-19 04:43:31 -0700

    added individual message translation

---
+++ src/components/Message.js
@@ -0,0 +1,99 @@
+import { useState } from 'react'
+import { Avatar } from '@chakra-ui/avatar'
+import { Tooltip } from '@chakra-ui/tooltip'
+import { useEffect } from 'react'
+import {
+    isLastMessage,
+    isSameSender,
+    isSameSenderMargin,
+    isSameUser,
+} from '../config/ChatLogics'
+
+const Message = ({ messages, i, m, user_id }) => {
+    const [translation, setTranslation] = useState('')
+
+    const getTranslation = async (text) => {
+        const res = await fetch('https://libretranslate.de/translate', {
+            method: 'POST',
+            body: JSON.stringify({
+                q: text,
+                source: 'en',
+                target: 'hi',
+                format: 'text',
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        let data = await res.json()
+
+        setTranslation(data.translatedText)
+        console.log(data.translatedText)
+    }
+
+    return (
+        <div style={{ display: 'flex' }} key={m._id}>
+            {(isSameSender(messages, m, i, user_id) ||
+                isLastMessage(messages, i, user_id)) && (
+                <Tooltip
+                    label={m.sender.name}
+                    placement="bottom-start"
+                    hasArrow
+                >
+                    <Avatar
+                        mt="7px"
+                        mr={1}
+                        size="sm"
+                        cursor="pointer"
+                        name={m.sender.name}
+                        src={m.sender.pic}
+                    />
+                </Tooltip>
+            )}
+
+            <span
+                style={{
+                    backgroundColor: `${
+                        m.sender._id === user_id ? '#BEE3F8' : '#B9F5D0'
+                    }`,
+                    marginLeft: isSameSenderMargin(messages, m, i, user_id),
+                    marginTop: isSameUser(messages, m, i, user_id) ? 3 : 10,
+                    borderRadius: '20px',
+                    padding: '5px 15px',
+                    maxWidth: '75%',
+                }}
+            >
+                {m.sender._id !== user_id && (
+                    <>
+                        <span
+                            style={{
+                                textDecoration: 'underline',
+                            }}
+                        >
+                            {m.sender.name}
+                            <button onClick={() => getTranslation(m.content)}>
+                                Translate
+                            </button>
+                        </span>
+                        <br />
+                    </>
+                )}
+                {m.content}
+
+                {translation && (
+                    <>
+                        <br />
+                        <span
+                            style={{
+                                color: 'grey',
+                            }}
+                        >
+                            {translation}
+                        </span>
+                    </>
+                )}
+            </span>
+        </div>
+    )
+}
+
+export default Message
--- src/components/ScrollableChat.js
+++ src/components/ScrollableChat.js
@@ -1,13 +1,7 @@
-import { Avatar } from '@chakra-ui/avatar'
-import { Tooltip } from '@chakra-ui/tooltip'
 import ScrollableFeed from 'react-scrollable-feed'
-import {
-    isLastMessage,
-    isSameSender,
-    isSameSenderMargin,
-    isSameUser,
-} from '../config/ChatLogics'
 import { ChatState } from '../Context/ChatProvider'
+
+import Message from './Message'
 
 const ScrollableChat = ({ messages }) => {
     const { user } = ChatState()
@@ -16,62 +10,13 @@ const ScrollableChat = ({ messages }) => {
         <ScrollableFeed>
             {messages &&
                 messages.map((m, i) => (
-                    <div style={{ display: 'flex' }} key={m._id}>
-                        {(isSameSender(messages, m, i, user._id) ||
-                            isLastMessage(messages, i, user._id)) && (
-                            <Tooltip
-                                label={m.sender.name}
-                                placement="bottom-start"
-                                hasArrow
-                            >
-                                <Avatar
-                                    mt="7px"
-                                    mr={1}
-                                    size="sm"
-                                    cursor="pointer"
-                                    name={m.sender.name}
-                                    src={m.sender.pic}
-                                />
-                            </Tooltip>
-                        )}
-
-                        <span
-                            style={{
-                                backgroundColor: `${
-                                    m.sender._id === user._id
-                                        ? '#BEE3F8'
-                                        : '#B9F5D0'
-                                }`,
-                                marginLeft: isSameSenderMargin(
-                                    messages,
-                                    m,
-                                    i,
-                                    user._id
-                                ),
-                                marginTop: isSameUser(messages, m, i, user._id)
-                                    ? 3
-                                    : 10,
-                                borderRadius: '20px',
-                                padding: '5px 15px',
-                                maxWidth: '75%',
-                            }}
-                        >
-                            {m.sender._id !== user._id && (
-                                <>
-                                    <span
-                                        style={{
-                                            textDecoration: 'underline',
-                                        }}
-                                    >
-                                        {m.sender.name}
-                                    </span>
-                                    <br />
-                                </>
-                            )}
-
-                            {m.content}
-                        </span>
-                    </div>
+                    <Message
+                        key={i}
+                        messages={messages}
+                        i={i}
+                        m={m}
+                        user_id={user._id}
+                    />
                 ))}
         </ScrollableFeed>
     )
